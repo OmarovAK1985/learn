0c78061f00f8a353ab466d927b0117e65fccfc1a
OmarovAK <Omarov_Anuar@mail.ru>
2022-01-26 01:21:13 +0600

    ADD file extension

--- vk.py
+++ vk.py
@@ -35,6 +35,7 @@ class VK:
             list_name_files = []
             list_sizes = []
             count = 1
+            file_extension = None
             date = None
             for i in tqdm(my_list, desc='Получение ссылок и присвоение имен файлам'):
                 for k, v in i.items():
@@ -45,28 +46,39 @@ class VK:
                             for key, val in i_1.items():
                                 if val == 'z':
                                     list_url.append(i_1['url'])
+                                    file_extension = i_1['url'].split('.')[-1]
+                                    file_extension = file_extension.split('?')[0]
                                     list_sizes.append(val)
 
                     if k == 'likes':
                         for keys, vals in v.items():
                             if keys == 'count':
-                                name_file = f'{vals}'
-                                count = count + 1
+                                name_file = f'{vals}.{file_extension}'
                                 if name_file in list_name_files:
-                                    name_file = f'{name_file}_Date_upload_{date}'
+                                    name_file = f'{vals}_Date_upload_{date}.{file_extension}'
                                 list_name_files.append(name_file)
                 time.sleep(0.1)
+            p = 0
+            for i in list_name_files:
+                name_file = list_name_files.pop(p)
+                list_name_files.insert(p, f'Number_file_{count}_Count_likes_{name_file}')
+                p = p+1
+                count = count + 1
+
+            print(list_name_files)
             my_dict = dict(zip(list_url, list_name_files))
             list_json = []
             for i, k in zip(list_sizes, list_name_files):
                 json_dict = {'file_name': k, 'sizes': i}
                 list_json.append(json_dict)
 
+
             file = os.path.join(os.getcwd(), 'file.json')
             with open(file=file, mode='w') as file_json:
                 json.dump(list_json, file_json, ensure_ascii=False, indent=2)
             if len(my_dict) < len(my_list):
-                print (f'Обработано фотографий в количестве {len(my_dict)} шт. Фотографии плохого качества не были обработаны в количестве: {len(my_list) - len(my_dict)} шт.')
+                print(
+                    f'Обработано фотографий в количестве {len(my_dict)} шт. Фотографии плохого качества не были обработаны в количестве: {len(my_list) - len(my_dict)} шт.')
             else:
                 print(f'Фотографии в количестве {len(my_list)} были успешно обработаны')
 
